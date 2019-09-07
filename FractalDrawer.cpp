256bdbc3bbd495f08b5df77c5ea4c9371d54bd72
ori-frenkel <49747014+ori-frenkel@users.noreply.github.com>
2019-09-07 10:56:59 +0300

    adding the use of boost library (working on linux)

--- FractalDrawer.cpp
+++ FractalDrawer.cpp
@@ -1,13 +1,43 @@
 //
-// Created by Owner on 05/09/2019.
+// Created by Ori Frenkel on 05/09/2019.
 //
 #include <iostream>
-//#include <boost/filesystem.hpp>
-//#include "../cppEx1V2/boost_1_55_0/boost/filesystem.hpp"
-//#include "../cppEx1V2/boost_1_55_0/boost/filesystem/path.hpp"
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include "Fractal.h"
+#include <vector>
 
+using namespace boost::filesystem;
 
-//using namespace boost::filesystem;
+/**
+ *
+ * @param ch - char
+ * @return - true if its a didgit (0-9)
+ */
+bool isDigit(const char &ch)
+{
+    return(std::isdigit(ch));
+}
+
+bool isLegalFormat(std::string line, const char& typeOfFractal, const char& dimOfFractal)
+{
+    if(!isDigit(typeOfFractal) || line[1] != ',' || !isDigit(dimOfFractal))
+    {
+        return false;
+    }
+
+    const int typeOfFractalInt = typeOfFractal - '0';
+    const int dimfFractalInt = typeOfFractal - '0';
+    // fractal type should be between 1 and 3 (include) and dim should be between 1 and 6 (include)
+    if(typeOfFractalInt < 1 || typeOfFractalInt > 3 || dimfFractalInt < 1 || dimfFractalInt > 6)
+    {
+        return false;
+    }
+    return true;
+}
+
 // TODO : remmber, when open file to close the file at the end/ before exiting with error
 int main(int argc, char* argv[])
 {
@@ -18,22 +48,61 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
-    /*
-    path path(argv[1]);
+
+    path p(argv[1]);
     // checking if the path actually exist
-    if (!boost::filesystem::exists(argv[1]))
+    if (!exists(p))
     {
         std::cerr << "Invalid input " << std::endl;
         exit(EXIT_FAILURE);
     }
-    if (!is_regular_file(path) || is_directory(path))
+    if (!is_regular_file(p) || is_directory(p))
     {
         std::cerr << "Invalid input " << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    */
 
+
+    std::string line;
+    std::ifstream input(argv[1]);
+    std::vector<Fractal*> vec;
+
+    /* iterating line by line and creating vectors with all the Fractals (if one of the line is
+       in wrong format exit with error) */
+    while(getline(input, line))
+    {
+        // format should be exactly 3 chars (num,num)
+        if(line.length() != 3)
+        {
+            input.close();
+            std::cerr << "Invalid input " << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        char typeOfFractal = line[0];
+        char dimOfFractal = line[2];
+        if(!isLegalFormat(line, typeOfFractal, dimOfFractal))
+        {
+            input.close();
+            std::cerr << "Invalid input " << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        vec.push_back(FactoryFractal::createFractal((int) (typeOfFractal - '0'), \
+                                                    (int) (dimOfFractal - '0')));
+
+    }
+
+    // print the vector content with reverse order
+    for (auto iteratorFractal = vec.rbegin(); iteratorFractal != vec.rend(); ++iteratorFractal)
+    {
+        (*iteratorFractal)->draw();
+        std::cout << std::endl;
+    }
+    input.close();
+
+    // TODO : according to the school solution the end of the test.txt can be empty line.
 
     return 0;
 }
--- main2.cpp
+++ main2.cpp
@@ -4,25 +4,64 @@
 
 #include <fstream>
 #include <iostream>
-#include "main2.h"
 #include <ostream>
-using namespace std;
+#include "Fractal.h"
+#include <vector>
+
+
+
+
+bool isDigit(const char &ch)
+{
+    return(std::isdigit(ch));
+}
+
+bool isLegalFormat(std::string line, const char& typeOfFractal, const char& dimOfFractal)
+{
+    if(!isDigit(typeOfFractal) || line[1] != ',' || !isDigit(dimOfFractal))
+    {
+        return false;
+    }
+    return true;
+}
+
+
 int main()
 {
-    //std::ifstream input( "C:\\Users\\Owner\\CLionProjects\\cppEx2\\test.txt" );
 
+    std::string line;
+    std::ifstream out("/cs/usr/ori.frenkel/CLionProjects/cppEx2S/test.txt");
+    std::vector<Fractal*> vec;
 
-    string line;
-    ifstream out("C:\\Users\\Owner\\CLionProjects\\cppEx2\\test.txt");
-    while(getline(out, line)) {
-        //cout << line << endl;
-        cout << line[3] << endl;
-        if(line[3] == '\n')
+    while(getline(out, line))
+    {
+        if(line.length() != 3)
         {
-            exit(100);
+            std::cerr << "Invalid input " << std::endl;
+            exit(EXIT_FAILURE);
         }
 
+        char typeOfFractal = line[0];
+        char dimOfFractal = line[2];
+        if(!isLegalFormat(line, typeOfFractal, dimOfFractal))
+        {
+            std::cerr << "Invalid input " << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        vec.push_back(FactoryFractal::createFractal((int) (typeOfFractal - '0'), (int) (dimOfFractal - '0')));
+
     }
+
+    // print the vector content with reverse order
+    for (auto iteratorFractal = vec.rbegin(); iteratorFractal != vec.rend(); ++iteratorFractal)
+    {
+        (*iteratorFractal)->draw();
+        std::cout << std::endl;
+    }
+
+
+
     out.close();
     return 0;
 }
